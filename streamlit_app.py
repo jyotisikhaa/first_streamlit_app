64421708c0cbf8d6ff89f3701900056686490f8f
jyotisikhaa <145773747+jyotisikhaa@users.noreply.github.com>
2023-09-27 11:12:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,115 @@
+[10:35] Somya Suyasha
+
 import streamlit
 
+ 
+
 streamlit.title("My Parents new Healthy Diner")
+
+ 
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.text(' 🥑🍞 Avacado Toast')
+
+ 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+ 
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+ 
+
+# Let's put a pick list here so they can pick the fruit they want to include
+
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+ 
+
+# Display the table on the page.
+
+streamlit.dataframe(fruits_to_show)
+
+ 
+
+# New section to display api response
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+ 
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
+streamlit.write('The user entered ', fruit_choice)
+
+ 
+
+ 
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+
+ 
+
+ 
+
+# write your own comment -what does the next line do? 
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# write your own comment - what does this do?
+
+streamlit.dataframe(fruityvice_normalized)
+
+ 
+
+#don't run anything past here while we troubleshoot
+
+streamlit.stop();
+
+ 
+
+import snowflake.connector
+
+ 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+my_cur = my_cnx.cursor()
+
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+
+my_data_rows = my_cur.fetchall()
+
+streamlit.header("THE FRUIT LOAD LIST CONTAINS:")
+
+streamlit.dataframe(my_data_rows)
+
+ 
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+
+ 
+
+streamlit.write('Thanks for adding',add_my_fruit)
+
+ 
+
+#this will not work correctly, but go with the flow
+
+my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES('FROM STREAMLIT')")
