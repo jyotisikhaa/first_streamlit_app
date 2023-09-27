f714da03c7fd10dadcc8b4b2acba8bf12ce6456e
Jyotisikhaa <145773747+jyotisikhaa@users.noreply.github.com>
2023-09-27 14:53:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
  
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 
 streamlit.write('The user entered ', fruit_choice)
 
